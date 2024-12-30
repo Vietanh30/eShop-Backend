1673a67a4193a77f5a88a16bff1ecd0c99733787
VietAnh30 <vietanhdang487@gmail.com>
2024-12-30 22:12:38 +0700

    fix

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,13 +1,42 @@
 const asyncHandler = require("express-async-handler");
 const categoriesModel = require("../models/categoriesModel.js");
+const productsModel = require("../models/productModel.js"); // Giả sử bạn có model cho sản phẩm
 
 class CategoryController {
-  //  [ GET - ROUTE: api/cart]
+  //  [ GET - ROUTE: api/cate]
   getAll = asyncHandler(async (req, res) => {
     const categories = await categoriesModel.find();
     res.json(categories);
   });
 
+  //  [ GET - ROUTE: api/cate/:id]
+  getCateById = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const category = await categoriesModel.findById(id);
+
+    if (!category) {
+      return res.status(404).json({ message: "Danh mục không tồn tại." });
+    }
+
+    res.json(category);
+  });
+
+  //  [ GET - ROUTE: api/cate/:slug]
+  getProductBySlugCate = asyncHandler(async (req, res) => {
+    const { slug } = req.params; // Lấy slug từ tham số đường dẫn
+
+    // Tìm danh mục theo slug
+    const category = await categoriesModel.findOne({ slug: slug });
+
+    if (!category) {
+      return res.status(404).json({ message: "Danh mục không tồn tại." });
+    }
+
+    // Tìm sản phẩm thuộc danh mục
+    const products = await productsModel.find({ cate: category._id }); // Giả sử sản phẩm có trường categoryId
+
+    res.json(products); // Trả về danh sách sản phẩm
+  });
 }
 
 module.exports = new CategoryController();
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,5 +3,6 @@ const categoryController = require("../controllers/categoryController");
 const router = express.Router();
 
 router.get("/", categoryController.getAll);
+router.get("/:slug", categoryController.getProductBySlugCate);
 
 module.exports = router;
